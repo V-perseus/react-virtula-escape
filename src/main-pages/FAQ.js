753eaad560038e45e777cc34bad92403de06774e
mango-jam-0513 <Sghttp@gmail.com>
2021-01-05 13:22:23 +0800

    update

--- src/main-pages/FAQ.js
+++ src/main-pages/FAQ.js
@@ -7,7 +7,7 @@ function FAQ() {
     return (
         <div className="container-fluid faq p-0 m-0 ">
             <div className="faq-bg">
-                <h1>FAQ</h1>
+                {/* <h1>FAQ</h1> */}
             </div>
             <div className="container mt-5 mb-5">
                 <div className="row p-0 m-0">
